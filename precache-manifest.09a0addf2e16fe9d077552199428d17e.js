fbfc97de5a16c042a9cb36b76dc0a06f62d7e5d7
Kush Gupta <reachkush.gupta@gmail.com>
2020-04-08 09:59:13 +0530

    Updates

---
+++ precache-manifest.09a0addf2e16fe9d077552199428d17e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356452acf96368b15a95142b52e60dba",
+    "url": "/reactauth/index.html"
+  },
+  {
+    "revision": "3a24217e3506e2bd1086",
+    "url": "/reactauth/static/css/main.c910d6f0.chunk.css"
+  },
+  {
+    "revision": "04dd46be210f6faf948b",
+    "url": "/reactauth/static/js/2.ede85560.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactauth/static/js/2.ede85560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a24217e3506e2bd1086",
+    "url": "/reactauth/static/js/main.a6c212b4.chunk.js"
+  },
+  {
+    "revision": "4d79feb3cbeaa49cd7c3",
+    "url": "/reactauth/static/js/runtime-main.86c6682f.js"
+  }
+]);
